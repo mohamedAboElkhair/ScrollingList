5d6e7b1f2c473e29a9828060efa92283ab43c9ae
MohamedAbo Elkhair <mohamed741987@gmail.com>
2020-09-03 11:52:57 +0200

    SetupCollectionView

---
+++ homeWorkOut/Model/ListData.swift
@@ -0,0 +1,9 @@
+//
+//  ListData.swift
+//  homeWorkOut
+//
+//  Created by Mohamed AbouElkhair on 8/30/20.
+//  Copyright © 2020 Mohamed AbouElkhair. All rights reserved.
+//
+
+import Foundation
---
+++ homeWorkOut/View/CvCell.swift
@@ -0,0 +1,9 @@
+//
+//  CVCell.swift
+//  homeWorkOut
+//
+//  Created by Mohamed AbouElkhair on 8/30/20.
+//  Copyright © 2020 Mohamed AbouElkhair. All rights reserved.
+//
+
+import Foundation
