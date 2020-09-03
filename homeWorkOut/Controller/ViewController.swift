43353543a0ab1d3c68b3b1bb90c389b29b8bd820
MohamedAbo Elkhair <mohamed741987@gmail.com>
2020-09-03 13:12:09 +0200

    ScrollingList UIViewController Done

--- homeWorkOut/Controller/ViewController.swift
+++ homeWorkOut/Controller/ViewController.swift
@@ -4,19 +4,18 @@
 //
 //  Created by Mohamed AbouElkhair on 3/09/20.
 //  Copyright © 2020 Mohamed AbouElkhair. All rights reserved.
-//
+//  http://italkwithcomputer.com
 
 import UIKit
 
 class HomeWorkOut: UIViewController {
 
     let dataList = [
-            LisData(titel: "The First", image: #imageLiteral(resourceName: "imgeList1"), url: "/goF"),
-            LisData(titel: "The Second", image: #imageLiteral(resourceName: "ImgaeList2"), url: "/goS"),
-            LisData(titel: "the Third", image: #imageLiteral(resourceName: "imgeList3"), url: "/goT")
+            LisData(titel: "The First", image: #imageLiteral(resourceName: "ImgaeList2"), url: "/goF"),
+            LisData(titel: "The Second", image: #imageLiteral(resourceName: "imgeList1"), url: "/goS"),
+            LisData(titel: "the Third", image: #imageLiteral(resourceName: "imgeList3"), url: "/goT"),
+            LisData(titel: "", image: #imageLiteral(resourceName: "imgaLest4"), url: "/go")
     ]
-    
-    
     fileprivate let collectionView: UICollectionView = {
     let layout = UICollectionViewFlowLayout()
     layout.scrollDirection = .horizontal
@@ -26,20 +25,15 @@ class HomeWorkOut: UIViewController {
         
     return cv
     }()
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "Home"
+        //view.backgroundColor = .blue
         SetupCollectionView()
     }
-
-
 }
-
-
 extension HomeWorkOut : UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
-     
+     // here you can chenge the width 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize (width: collectionView.frame.width/2.5, height: collectionView.frame.width/2)
     }
@@ -61,7 +55,15 @@ extension HomeWorkOut : UICollectionViewDelegateFlowLayout, UICollectionViewData
 extension HomeWorkOut {
     
     func SetupCollectionView (){
-        view.addSubview(collectionView)
+    view.addSubview(collectionView)
+    collectionView.backgroundColor = .clear
+    collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+    collectionView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 5).isActive = true
+    collectionView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 100).isActive = true
+    collectionView.heightAnchor.constraint(equalTo: collectionView.widthAnchor, multiplier: 0.5).isActive = true
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
     }
     
     
--- homeWorkOut/View/CvCell.swift
+++ homeWorkOut/View/CvCell.swift
@@ -11,12 +11,28 @@ import UIKit
 class CvCell: UICollectionViewCell {
     var dataListImage : LisData? {
         didSet{
+            guard let dataListIamaga = dataListImage else {return}
+            imageList.image = dataListIamaga.image
             
         }
     }
+    
+    fileprivate let imageList : UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(named: "imgeList1")
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds =  true
+        iv.layer.cornerRadius = 15
+        return iv
+    }()
     override init(frame: CGRect) {
            super.init(frame: .zero)
-         
+        contentView.addSubview(imageList)
+        imageList.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        imageList.leftAnchor.constraint(equalTo: contentView.leftAnchor).isActive = true
+        imageList.rightAnchor.constraint(equalTo: contentView.rightAnchor).isActive = true
+        imageList.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
            
            
        }
